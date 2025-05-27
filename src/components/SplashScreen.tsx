cc6c5a39e737a354d6ad2f1efb7c3cbc515c970b
Chhavi Nayyar <115122937+chhaviiiii@users.noreply.github.com>
2025-05-27 16:50:52 -0700

    Clean up  and prep for deploy

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import TypeWriter from "./TypeWriter";
-import Image from "next/image";
 
 interface SplashScreenProps {
   onFinish: () => void;

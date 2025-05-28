12dcb6acdda61142e90a19f243b97e8ceda77579
Chhavi Nayyar <115122937+chhaviiiii@users.noreply.github.com>
2025-05-28 12:56:02 -0700

    Clean up  and prep for deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
     <head>
       <title>Chhavi Nayyar | Portfolio</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
